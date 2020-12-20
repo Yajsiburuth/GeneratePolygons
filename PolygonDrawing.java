aa695549cd0c29abb8c5d74cd05be94c892e8836
Yaj Siburuth <52027741+Yajsiburuth@users.noreply.github.com>
2020-12-20 14:13:15 +0400

    Add files via upload

--- PolygonDrawing.java
+++ PolygonDrawing.java
@@ -10,7 +10,7 @@ public class PolygonDrawing extends Frame{
 	Scanner sc = new Scanner(System.in);
 	private String numSide = JOptionPane.showInputDialog(null, "Enter line number");
 	private Color lineColor = JColorChooser.showDialog(null, "Choose a color for the lines of the shape", Color.black);
-	private Color fillColor = JColorChooser.showDialog(null, "Choose a color to fill the shape with", Color.black);
+	private Color fillColor = JColorChooser.showDialog(null, "Choose a color to fill the shape with	", Color.black);
 	public void paint(Graphics g) {
 		int numSides = Integer.parseInt(numSide);
 		Graphics2D ga = (Graphics2D) g;
@@ -23,6 +23,7 @@ public class PolygonDrawing extends Frame{
 	      p.addPoint((int) (100 + 50 * Math.cos(i * 2 * Math.PI / numSides)),
 	          (int) (100 + 50 * Math.sin(i * 2 * Math.PI / numSides)));
 		ga.setPaint(lineColor);
+		ga.setStroke(new BasicStroke(5));
 		ga.draw(p);
 		ga.setPaint(fillColor);
 		ga.fill(p);
