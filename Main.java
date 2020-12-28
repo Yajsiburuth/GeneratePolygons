8bfedb3f739fb80fd2f06c73138397ec36fcf9e1
Yaj Siburuth <52027741+Yajsiburuth@users.noreply.github.com>
2020-12-28 10:23:45 +0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,17 +2,111 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.*;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Frame frame = new PolygonDrawing();
-		frame.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent we) {
-				System.exit(0);
+        new Main();
+    }
+	public Main() {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				JFrame frameOut = new JFrame("Polygon");
+				JFrame frameIn = new JFrame("Settings");
+			
+				JPanel panel = new JPanel();
+				panel.setLayout(new GridLayout(3,1,1,1));
+				frameOut.setSize(852,480);	
+				PolygonDrawing polygon = new PolygonDrawing();
+				
+				JSlider sliderLines = new JSlider(0,30);
+				JSlider sliderThick = new JSlider(0,50);
+				JLabel sliderLabel = new JLabel("Number of lines: 4",JLabel.CENTER); 
+				JLabel sliderLabel1 = new JLabel("Thickness of line : 10 ", JLabel.CENTER);
+				sliderLines.setMinorTickSpacing(1);  
+				sliderLines.setMajorTickSpacing(5);  
+				sliderLines.setPaintTicks(true);  
+				sliderLines.setPaintLabels(true); 
+				sliderLines.setSnapToTicks(true);
+				sliderLines.setValue(4);
+				
+				sliderThick.setMinorTickSpacing(1);  
+				sliderThick.setMajorTickSpacing(5);  
+				sliderThick.setPaintTicks(true);  
+				sliderThick.setPaintLabels(true); 
+				sliderThick.setValue(10);
+				
+				JButton regular = new JButton("Regular");
+				regular.setBackground(Color.cyan);
+				JButton irregular = new JButton("Irregular");
+				
+				panel.add(sliderLabel);
+				panel.add(sliderLines);
+				panel.add(sliderLabel1);
+				panel.add(sliderThick);
+				panel.add(regular);
+				panel.add(irregular);
+				
+				frameIn.add(panel, BorderLayout.CENTER);
+				frameIn.setSize(600,250);
+				frameIn.setVisible(true);
+				frameIn.setLayout(new BorderLayout());
+				
+				sliderLines.addChangeListener(new ChangeListener() {
+					@Override
+					public void stateChanged(ChangeEvent arg0) {
+						int sides = sliderLines.getValue();
+						sliderLabel.setText("Number of lines: " + Integer.toString(sides));
+		                polygon.setSides(sides);
+					}
+				});
+				
+				sliderThick.addChangeListener(new ChangeListener() {
+					@Override
+					public void stateChanged(ChangeEvent arg0) {
+						int thick = sliderThick.getValue();
+						sliderLabel1.setText("Thickness of line: " + Integer.toString(thick));
+		                polygon.setThick(thick);
+					}
+				});
+				
+				regular.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						irregular.setBackground(null);
+						regular.setBackground(Color.cyan);
+						
+						System.out.print(0);
+		                polygon.setType(0);
+					}
+				});
+				
+				irregular.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						regular.setBackground(null);
+						irregular.setBackground(Color.cyan);
+						System.out.print(1);
+		                polygon.setType(1);
+					}
+				});
+				
+				//frameOut.pack();
+		        frameOut.setLocation(800,200);
+		        frameOut.getContentPane().add(polygon);
+		        frameOut.setVisible(true);
 			}
 		});
-		frame.setSize(1280, 720);
-		frame.setVisible(true);
+		
 	}
-
 }
--- PolygonDrawing.java
+++ PolygonDrawing.java
@@ -3,23 +3,38 @@ import java.awt.event.*;
 import java.awt.geom.*;
 import java.awt.geom.Rectangle2D.Double;
 import java.util.Random;
-import java.util.Scanner;
 
 import javax.swing.JColorChooser;
 import javax.swing.JOptionPane;
-public class PolygonDrawing extends Frame{
-	Scanner sc = new Scanner(System.in);
-	Object[] options = {"Regular", "Irregular"};
-	int x = JOptionPane.showOptionDialog(null, "Choose one",
-            "Polygon chooser",
-            JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE, null, options, options[0]);
-	private String numSide = JOptionPane.showInputDialog(null, "Enter line number");
-	private Color lineColor = JColorChooser.showDialog(null, "Choose a color for the lines of the shape", Color.black);
+import javax.swing.JPanel;
+public class PolygonDrawing extends JPanel{
+	int x = 0;
+	int numSides = 4;
+	int thickness = 10;
+	private Color lineColor = JColorChooser.showDialog(null, "Choose a color for the lines of the shape", Color.GRAY);
 	private Color fillColor = JColorChooser.showDialog(null, "Choose a color to fill the shape with	", Color.black);
 	
-	public void paint(Graphics g) {
-		int numSides = Integer.parseInt(numSide);
-		Graphics2D ga = (Graphics2D) g;
+	@Override
+    public Dimension getPreferredSize() {
+        return new Dimension(852, 480);
+    }
+	public void setSides(int sides) {
+        numSides = sides;
+        repaint();
+    }
+	public void setThick(int thick) {
+		thickness = thick;
+        repaint();
+    }
+	public void setType(int type) {
+		x = type;
+		repaint();
+		
+	}
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D ga = (Graphics2D) g.create();
 		RenderingHints rh = new RenderingHints(
 	             RenderingHints.KEY_ANTIALIASING,
 	             RenderingHints.VALUE_ANTIALIAS_ON);
@@ -27,15 +42,10 @@ public class PolygonDrawing extends Frame{
 		Polygon p = new Polygon();
 		if (x == 0) {
 			for (int i = 0; i < numSides; i++) {
-				p.addPoint((int) (610 + 100 * Math.cos(i * 2 * Math.PI / numSides)),
-				          (int) (360 + 100 * Math.sin(i * 2 * Math.PI / numSides)));
+				p.addPoint((int) (426 + 100 * Math.cos(i * 2 * Math.PI / numSides)),
+				          (int) (240 + 100 * Math.sin(i * 2 * Math.PI / numSides)));
 				
-			}   
-			ga.setPaint(lineColor);
-			ga.setStroke(new BasicStroke(10));
-			ga.draw(p);
-			ga.setPaint(fillColor);
-			ga.fill(p);
+			}
 		}else {
 			int minX = 100;
 			int maxX = 300;
@@ -44,15 +54,18 @@ public class PolygonDrawing extends Frame{
 			for (int i = 0; i < numSides; i++) {
 				int newRandX = (int)(Math.random() * (maxX - minX + 1) + minX);
 				int newRandY = (int)(Math.random() * (maxY - minY + 1) + minY);
-				p.addPoint((int) (610 + newRandX * Math.cos(i * 2 * Math.PI / numSides)),
-				          (int) (360 + newRandY * Math.sin(i * 2 * Math.PI / numSides)));
+				p.addPoint((int) (426 + newRandX * Math.cos(i * 2 * Math.PI / numSides)),
+				          (int) (240 + newRandY * Math.sin(i * 2 * Math.PI / numSides)));
 			}     
-			ga.setPaint(lineColor);
-			ga.setStroke(new BasicStroke(10));
-			ga.draw(p);
-			ga.setPaint(fillColor);
-			ga.fill(p);
+			
+			
 		}
+		ga.setPaint(lineColor);
+		ga.setStroke(new BasicStroke(thickness));
+		ga.draw(p);
+		ga.setPaint(fillColor);
+		ga.fill(p);
+		ga.dispose();
 	    
 	}
 }
